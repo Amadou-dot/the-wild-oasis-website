2781fa71f096e8f44d148107c2fed0d111532046
Amadou-dot <89759636+Amadou-dot@users.noreply.github.com>
2025-02-10 01:40:07 -0700

    Add initial components and pages for the application

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'dclaevazetcjjkrzczpc.supabase.co',
+        port: '',
+        // pathname: '/storage/v1/object/public/cabin-images/cabin-001.jpg',
+        search: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'flagcdn.com',
+        port: '',
+        // pathname: '/w20/sn.png',
+        search: '',
+      },
+    ],
+  },
+}
 
 export default nextConfig;
---
+++ src/app/account/page.tsx
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: 'Guest area',
+  description: 'Your account page',
+};
+export default function page() {
+  return <h1 className="font-semibold text-2xl text-accent-400 mb-7">Welcome, Amadou</h1>;
+}

adb4cf76fb3e8f56c8886596a35b69cd59133252
Amadou-dot <89759636+Amadou-dot@users.noreply.github.com>
2025-02-18 13:47:46 -0700

    Update placeholder text and adjust layout in UpdateProfileForm component

--- src/components/UpdateProfileForm.tsx
+++ src/components/UpdateProfileForm.tsx
@@ -33,11 +33,11 @@ export default function UpdateProfileForm() {
 
       <FormInput
         label='National ID number'
-        placeholder='Enter your first name'
+        placeholder=' Your National ID number'
         name='national_id'
       />
 
-      <div className='flex justify-end items-center gap-6'>
+      <div className='flex justify-end items-center gap-6 w-full'>
         <Button
           className='bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300 rounded-none'
           size='lg'
